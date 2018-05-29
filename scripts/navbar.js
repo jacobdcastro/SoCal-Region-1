76903eab484d9c238db9968a6a29658fb1180669
Jacob D. Castro <jdcastro.business@gmail.com>
2018-05-29 15:41:46 -0700

    Initial Commit

---
+++ scripts/navbar.js
@@ -0,0 +1,23 @@
+var menuButton = document.getElementById("mobileMenuButton");
+var closeButton = document.getElementById("closeButton");
+var w = window.innerWidth;
+var opened = false;
+console.log(w);
+
+/* Open the sidenav */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+    opened = true;
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    opened = false;
+}
+
+menuButton.addEventListener("mousedown", openNav);
+closeButton.addEventListener("mousedown", closeNav);
+
+menuButton.addEventListener("touchend", openNav);
+closeButton.addEventListener("touchend", closeNav);
