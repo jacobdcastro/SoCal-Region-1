1c89af21e3aa31cf239c718a07c0b44931c838c2
jacobdcastro <jdcastro.business@gmail.com>
2018-07-15 21:58:29 -0700

    add event listeners to .speaker-title classes

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -16,16 +16,27 @@ function closeNav() {
     opened = false;
 }
 
+// event listeners for mouse clicks
 menuButton.addEventListener("mousedown", openNav);
 closeButton.addEventListener("mousedown", closeNav);
 
+// event listeners for touch screens
 menuButton.addEventListener("touchend", openNav);
 closeButton.addEventListener("touchend", closeNav);
 
 // Div click test ============================================
+var title = document.getElementsByClassName('speaker-title');
+var speaker = document.getElementsByClassName('speaker');
+var opened = false;
+console.log(title);
 
-var speaker = document.getElementById('johnMartin');
-
-speaker.addEventListener('onclick', function () {
-  console.log('Hello, world!');
-});
+for (let i = 0; i < 6; i++) {
+    title[i].addEventListener('mousedown', function () {
+      if (title[i].style.backgroundColor !== 'blue') {
+        title[i].style.backgroundColor = 'blue';
+      } else {
+        title[i].style.backgroundColor = 'black';
+      }
+    });
+}
+// instead of checking if opened is true or false, try measuring height of div
