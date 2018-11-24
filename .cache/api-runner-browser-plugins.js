fe849d06ad6bd4ec648d6f5f41b232039de76240
Jacob D. Castro <jdcastro.business@gmail.com>
2018-11-24 12:22:05 -0800

    initial commit

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('C:/Users/jdcas/jdcastro-web-dd/socal-region-1/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/async-requires.js
@@ -0,0 +1,12 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("C:\\Users\\jdcas\\jdcastro-web-dd\\socal-region-1\\.cache\\dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("C:\\Users\\jdcas\\jdcastro-web-dd\\socal-region-1\\src\\pages\\404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-index-js": () => import("C:\\Users\\jdcas\\jdcastro-web-dd\\socal-region-1\\src\\pages\\index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-page-2-js": () => import("C:\\Users\\jdcas\\jdcastro-web-dd\\socal-region-1\\src\\pages\\page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */)
+}
+
+exports.data = () => import("C:\\Users\\jdcas\\jdcastro-web-dd\\socal-region-1\\.cache\\data.json")
+
