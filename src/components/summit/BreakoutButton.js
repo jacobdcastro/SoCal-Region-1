1e79fa588dcda05addb407c7d04684b6ae69e388
Jacob D. Castro <jdcastro.business@gmail.com>
2018-12-18 01:58:19 -0800

    create new Button component

---
+++ src/components/summit/BreakoutButton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class BreakoutButton extends React.Component {
+   render() {
+      const data = this.props.speakerData;
+      return (
+         <button id={this.props.idName} className="description-card" >
+            <h2>{data.firstName} {data.lastName}</h2>
+            <h3>{data.breakoutTitle}</h3>
+            <p>Click for more info</p>
+         </button>
+      );
+   }
+}
+
+export default BreakoutButton;
